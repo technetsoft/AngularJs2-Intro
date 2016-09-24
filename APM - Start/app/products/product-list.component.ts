bf65c68b985e409d3a5d24c6f6eb6e37d5ad8d7c
riyant <riyant.kurniawan@gmail.com>
2016-09-24 21:42:44 +0800

    create and implement interface

--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from 'angular2/core';
 
+import { IProduct } from './iproduct';
+
 @Component({
     selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html'
@@ -11,7 +13,7 @@ export class ProductListComponent {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
